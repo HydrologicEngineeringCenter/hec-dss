8afc92f9cc7562f069cbaf489be6b73cc1d94ace
Neema Nejad <37365315+sharkcooper@users.noreply.github.com>
2021-07-27 16:04:17 -0700

    removed standalone-test and test projects.
    added DotNetTests project with files from standalone-test.
    
    fixed TestUtility.basepath variable.

--- dotnet/DotNetTests/Properties/AssemblyInfo.cs
+++ dotnet/DotNetTests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Standalone-test")]
+[assembly: AssemblyTitle("DotNetTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Standalone-test")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("DotNetTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("79f7ea88-1ab6-4293-accc-464b0ff3f179")]
+[assembly: Guid("c4e4d3c7-2b03-4d2e-9c57-faac32483764")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
