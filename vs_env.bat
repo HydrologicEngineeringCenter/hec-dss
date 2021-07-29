afacd6aa23aad7f3a72508e5aef41e44c467c53b
Neema Nejad <37365315+sharkcooper@users.noreply.github.com>
2021-07-29 12:31:15 -0700

    removed comment in environment batch file

--- vs_env.bat
+++ vs_env.bat
@@ -1,2 +1,1 @@
-::call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2019.1.144\windows\bin\ipsxe-comp-vars.bat" intel64 vs2017
 call "%ONEAPI_ROOT%compiler\latest\env\vars.bat" intel64 vs2019
