8abb560003b3638f877d66e79258d2b2c74b830a
Neema Nejad <37365315+sharkcooper@users.noreply.github.com>
2021-07-29 12:56:55 -0700

    removed echo in environment batch file

--- vs_env.bat
+++ vs_env.bat
@@ -1,2 +1,1 @@
-echo %%ONEAPI_ROOT%%
 call "%%ONEAPI_ROOT%%\compiler\latest\env\vars.bat" intel64 vs2019
--- vs_env32.bat
+++ vs_env32.bat
@@ -1,2 +1,1 @@
-echo %%ONEAPI_ROOT%%
 call "%%ONEAPI_ROOT%%compiler\latest\env\vars.bat" ia32 vs2019
