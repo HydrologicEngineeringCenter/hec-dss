7ed2f3f8371a3882bd2d3100856ac917a051406f
Neema Nejad <37365315+sharkcooper@users.noreply.github.com>
2021-09-01 14:55:10 -0700

    added additional program name tests

--- dotnet/DotNetTests/ProgramNameTesting.cs
+++ dotnet/DotNetTests/ProgramNameTesting.cs
@@ -44,29 +44,35 @@ namespace DSSUnitTests
             }
         }
 
-        //[TestMethod]
-        //public void SetProgramNameForMultipleRecords_Dss6()
-        //{
-        //    string orig = TestUtility.BasePath + "simpleQCT6_RTS.dss";
-        //    string fn = TestUtility.BasePath + "simpleQCT6Copy_RTS.dss";
-        //    File.Delete(fn);
-        //    File.Copy(orig, fn);
-        //    using (DssWriter w = new DssWriter(fn))
-        //    {
-        //        w.SetProgramName("DOTNET");
-        //        for (int i = 0; i < 10; i++)
-        //        {
-        //            var ts = TimeSeriesTest.CreateSampleTimeSeries(new DateTime(2020, 1, 1), "cfs", "INST", size: 10);
-        //            ts.Path = new DssPath("a" + i.ToString(), "b", "c", "", E: "1Day", F: "f");
-        //            w.Write(ts);
-        //        }
-        //    }
+        [TestMethod]
+        public void SetProgramNameForMultipleRecords_Dss6()
+        {
+            string orig = TestUtility.BasePath + "simpleQCT6_RTS.dss";
+            string fn = TestUtility.BasePath + "simpleQCT6Copy_RTS.dss";
+            File.Delete(fn);
+            File.Copy(orig, fn);
+            List<DssPath> paths = new List<DssPath>();
+            using (DssWriter w = new DssWriter(fn))
+            {
+                w.SetProgramName("DOTNET");
+                for (int i = 0; i < 10; i++)
+                {
+                    var ts = TimeSeriesTest.CreateSampleTimeSeries(new DateTime(2020, 1, 2), "cfs", "INST", size: 10);
+                    ts.Path = new DssPath("a" + i.ToString(), "b", "c", "01Jan2020", E: "1Day", F: "f");
+                    paths.Add(ts.Path);
+                    w.Write(ts);
+                }
+            }
 
-        //    using (DssReader r = new DssReader(fn))
-        //    {
-        //        Assert.AreEqual("DOTNET", r.GetProgramName());
-        //    }
-        //}
+            using (DssReader r = new DssReader(fn))
+            {
+                foreach (DssPath path in paths)
+                {
+                    var ts = r.GetTimeSeries(path);
+                    Assert.AreEqual("DOTNET", ts.ProgramName);
+                }
+            }
+        }
 
         [TestMethod]
         public void SetProgramName_Dss7()
@@ -86,6 +92,78 @@ namespace DSSUnitTests
             {
                 var temp = r.GetTimeSeries(ts.Path);
                 Assert.AreEqual("dotnet", temp.ProgramName);
+            }
+        }
+
+        [TestMethod]
+        public void SetProgramNameForMultipleRecords_Dss7()
+        {
+            string fn = TestUtility.BasePath + "simpleQCT7.dss";
+            File.Delete(fn);
+            List<DssPath> paths = new List<DssPath>();
+            using (DssWriter w = new DssWriter(fn))
+            {
+                w.SetProgramName("dotnet-test");
+                for (int i = 0; i < 10; i++)
+                {
+                    var ts = TimeSeriesTest.CreateSampleTimeSeries(new DateTime(2020, 1, 2), "cfs", "INST", size: 10);
+                    ts.Path = new DssPath("a" + i.ToString(), "b", "c", "01Jan2020", E: "1Day", F: "f");
+                    paths.Add(ts.Path);
+                    w.Write(ts);
+                }
+            }
+
+            using (DssReader r = new DssReader(fn))
+            {
+                foreach (DssPath path in paths)
+                {
+                    var ts = r.GetTimeSeries(path);
+                    Assert.AreEqual("dotnet-test", ts.ProgramName);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void SetProgramNameAndSaveAsFloat_Dss6()
+        {
+            string orig = TestUtility.BasePath + "simpleQCT6_RTS.dss";
+            string fn = TestUtility.BasePath + "simpleQCT6Copy_RTS.dss";
+            File.Delete(fn);
+            File.Copy(orig, fn);
+
+            var ts = TimeSeriesTest.CreateSampleTimeSeries(new DateTime(2020, 1, 2), "cfs", "INST", size: 10);
+            ts.Path = new DssPath("A", "B", "C", "01JAN2020", E: "1DAY", F: "F");
+            using (DssWriter w = new DssWriter(fn))
+            {
+                w.SetProgramName("DOTNET");
+                w.Write(ts, true);
+            }
+
+            using (DssReader r = new DssReader(fn))
+            {
+                var temp = r.GetTimeSeries(ts.Path);
+                Assert.AreEqual("DOTNET", temp.ProgramName);
+            }
+        }
+
+        [TestMethod]
+        public void SetProgramNameAndSaveAsFloat_Dss7()
+        {
+            string fn = TestUtility.BasePath + "simpleQCT7.dss";
+            File.Delete(fn);
+
+            var ts = TimeSeriesTest.CreateSampleTimeSeries(new DateTime(2020, 1, 2), "cfs", "INST", size: 10);
+            ts.Path = new DssPath("a", "b", "c", "01Jan2020", E: "1Day", F: "f");
+            using (DssWriter w = new DssWriter(fn))
+            {
+                w.SetProgramName("dotnet-test");
+                w.Write(ts, true);
+            }
+
+            using (DssReader r = new DssReader(fn))
+            {
+                var temp = r.GetTimeSeries(ts.Path);
+                Assert.AreEqual("dotnet-test", temp.ProgramName);
             }
         }
     }
