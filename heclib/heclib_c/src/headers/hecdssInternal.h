7e8b3e454b2d7a1d1fda1d4257a5ff21ed59c1d0
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2022-03-21 14:35:57 -0700

    update DSS_VERSION_DATE

--- heclib/heclib_c/src/headers/hecdssInternal.h
+++ heclib/heclib_c/src/headers/hecdssInternal.h
@@ -37,7 +37,7 @@
 
 
 #define DSS_VERSION "7-II"
-#define DSS_VERSION_DATE "11 March 2022"
+#define DSS_VERSION_DATE "21 March 2022"
 
 
 const char *ztypeName(int recordType, int boolAbbreviation);
