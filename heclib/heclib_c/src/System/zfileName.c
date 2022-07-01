1504fcd1c850dd62f8764ed245b3ddad20257b13
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2022-07-01 12:03:39 -0700

    try stat instead of stat64

--- heclib/heclib_c/src/System/zfileName.c
+++ heclib/heclib_c/src/System/zfileName.c
@@ -61,9 +61,6 @@
 	struct __stat64 buf;
 #elif __linux__
 struct stat buf;
-#define stat64 stat
-#else
-	 struct stat64 buf;
 #endif
 	int result;
 	char name[_MAX_PATH];
@@ -167,7 +164,7 @@ struct stat buf;
 	 //  Determine if the file exists
 	 buf.st_size = 0;
 
-	result = stat64(fullDssFilename, &buf );
+	result = stat(fullDssFilename, &buf );
 	//  _stat throws an error if the file does not exist - reset it.
 	if (result == 0) {
 		//  Get the file permission
