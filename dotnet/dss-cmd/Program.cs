300bf551ab1c1ad541807d833b73007e2a44b709
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2022-09-23 09:22:56 -0700

    begin setup for tsGetSizes

--- dotnet/dss-cmd/Program.cs
+++ dotnet/dss-cmd/Program.cs
@@ -3,19 +3,6 @@ using System.Buffers;
 using System.Runtime.InteropServices;
 using System.Text;
 
-[DllImport("hecdss.dll")]
-static extern IntPtr hec_dss_open(string fileName);
-
-[DllImport("hecdss.dll")]
-static extern int hec_dss_close(IntPtr dss_file);
-
-[DllImport("hecdss.dll", CharSet= CharSet.Ansi, ExactSpelling = true)]
-static extern int hec_dss_tsRetrieve(IntPtr pdss, string pathname,
-                                  string startDate, string startTime,
-                                  string endDate,   string endTime,
-                                  int[] timeArray, double[] valueArray, int arraySize,
-                                  ref int numberValuesRead, ref int julianBaseDate,
-                                  byte[] units,int unitsLength, byte[] type, int typeLength);
 
 
 //dss_cmd.BasicPinvokeTests.Run();
@@ -23,10 +10,10 @@ static extern int hec_dss_tsRetrieve(IntPtr pdss, string pathname,
 
 
 string filename = "sample7.dss";
-var dss = hec_dss_open(filename);
+var dss = DssNative.hec_dss_open(filename);
 
 int buff_size = 5000;
-string path = "//SACRAMENTO/PRECIP-INC/01Jan1877/1Day/OBS/";
+string path = "/GREEN RIVER/GLENFIR/FLOW/01Apr1992/1Hour/OBS/";
 int[] times = new int[buff_size];
 double[] value = new double[buff_size];
 int numberValuesRead = 0;
@@ -37,9 +24,17 @@ var type = ArrayPool<byte>.Shared.Rent(32);
 
 string startDateTime = "01Jan1877 01:00";
 string endDateTime = "31Jan1877 24:00";
+string startDate = "";
+string startTime = "";
+string endDate = ""; //endDateTime.Split(' ')[0]
+string endTime = "";
 
-int status = hec_dss_tsRetrieve(dss, path, startDateTime.Split(' ')[0], startDateTime.Split(' ')[1],
-   endDateTime.Split(' ')[0], startDateTime.Split(' ')[1], times, value, buff_size, 
+int numberValues = 0;
+
+DssNative.hec_dss_tsGetSizes(dss, path, startDate,startTime,endDate, endTime, ref numberValues);
+Console.WriteLine(numberValues);
+
+int status = DssNative.hec_dss_tsRetrieve(dss, path, startDate, startTime, endDate, endTime, times, value, buff_size, 
    ref numberValuesRead, ref julianBaseDate,
    units,units.Length, type, type.Length);
 
@@ -51,4 +46,4 @@ ArrayPool<byte>.Shared.Return(units);
 ArrayPool<byte>.Shared.Return(type);
 
 
-hec_dss_close(dss);
+DssNative.hec_dss_close(dss);
