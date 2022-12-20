5dabde7f579576838ab1feea4e5c7e61e0a443c0
Karl Tarbet <ktarbet@users.noreply.github.com>
2022-12-20 11:03:40 -0800

    fix for DSS-139 (#168)
    * fix for DSS-139
    
    introducing jni_utility to simplify working with JNI calls
    
    * move jni_utility into src directory
    
    * update DSS_VERSION_DATE
    
    * more use of hec_dss_jni_setStringField
    
    * more use of hec_dss_jni_setStringField
    
    support case where field name may not exist.
    
    * add missing }

---
+++ heclib/javaHeclib/src/jni_utility.c
@@ -0,0 +1,22 @@
+#include "jni_utility.h"
+
+void hec_dss_jni_setStringField(JNIEnv *env, jclass cls, jobject obj, const char* name, const char* value) {
+	jstring jstr;
+	jfieldID fid = (*env)->GetFieldID(env, cls, name, "Ljava/lang/String;");
+	if ((*env)->ExceptionOccurred(env)) { // field may not exist
+		(*env)->ExceptionClear(env);
+		return;
+	}
+
+	if (fid) {
+		if (value) {
+			jstr = (*env)->NewStringUTF(env,value);
+		}
+		else {
+			jstr = (*env)->NewStringUTF(env, "");
+		}
+		(*env)->SetObjectField(env, obj, fid, jstr);
+		(*env)->DeleteLocalRef(env, jstr);
+
+	}
+}
