5f20016c93552b5d07323fc820bf490f25d6e624
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2024-08-19 13:08:46 -0700

    use size_t

--- heclib/heclib_c/src/headers/checks.h
+++ heclib/heclib_c/src/headers/checks.h
@@ -6,4 +6,4 @@
 void checkdoubles_(double* dataOrig, double* dataRead, int* number, char* mess, int* status, size_t dummy);
 void checkfloats_(float* dataOrig, float* dataRead, int* number, char* mess, int* status, size_t dummy);
 void checknumbers_(int* numberOrig, int* numberRead, const char* mess, int* status, size_t dummy);
-void checkints_(int* dataOrig, int* dataRead, int* length, int* number, char* mess, int* status, int mess_len);
+void checkints_(int* dataOrig, int* dataRead, int* length, int* number, char* mess, int* status, size_t mess_len);
