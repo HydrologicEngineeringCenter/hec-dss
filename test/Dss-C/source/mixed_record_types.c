2f9dbe60e04382af7ff7e8b3c372e17f784ba37a
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2024-09-27 10:05:14 -0700

    demonstrate data set with mixture of float/double

--- test/Dss-C/source/mixed_record_types.c
+++ test/Dss-C/source/mixed_record_types.c
@@ -5,11 +5,15 @@ int writeDoublesTimeSeries(long long* ifltab, const char* path, const char* date
 int writeSingleTimeSeries(long long* ifltab, const char* path, const char* date, const char* time);
 
 
-int test_mixed_record_types() {
+/// <summary>
+/// This test demonstrates a data-set (multip-records) can be a mixture of float,double
+/// </summary>
+/// <returns></returns>
+int create_mixed_record_types() {
 	long long ifltab[250];
-  const char* dssFileName = "test_mixed_record_types.dss";
+	const char* dssFileName = "test_create_mixed_record_types.dss";
 
-  deleteFile(dssFileName);
+	deleteFile(dssFileName);
 	int status = hec_dss_zopen(ifltab, dssFileName);
 	if (status != STATUS_OKAY) {
 		return status;
@@ -23,13 +27,36 @@ int test_mixed_record_types() {
 	if (status != STATUS_OKAY) {
 		return status;
 	}
+
+	status = writeDoublesTimeSeries(ifltab, path, "01Jun2026", time);
+
+	return status;
+}
+
+int test_mixed_record_types() {
+
+	long long ifltab[250];
+  const char* dssFileName = "test_mixed_record_types.dss";
+
+  deleteFile(dssFileName);
+	//int status = zopen6(ifltab, dssFileName);
+	int status = hec_dss_zopen(ifltab, dssFileName);
+	if (status != STATUS_OKAY) {
+		return status;
+	}
+	const char* date = "01Jun2024";
+	const char* time = "1200";
+
+	const char* path = "//GAPT_DAM/FLOW-LOCAL//1Hour/GAPT_HMS_FORECAST/";
+
+	status = writeSingleTimeSeries(ifltab, path, date, time);
+	if (status != STATUS_OKAY) {
+		return status;
+	}
 	status = writeDoublesTimeSeries(ifltab, path, date, time);
 
-
-  // re-write record (singles)
-
-  // read data to check
-
+	// TO DO  did the data get mangled?
+	
 	return status;
 }
 
